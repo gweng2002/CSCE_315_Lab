32806d6c4435f06846ada082974336680d0ebec0
gweng2002 <gweng2002@tamu.edu>
2022-09-14 22:19:47 -0500

    <first extra edits>

---
+++ public_html/function.js
@@ -0,0 +1,10 @@
+var num = 0;
+
+function style1()
+{
+    document.getElementById('page_style').setAttribute("href", "style1.css");
+}
+function style2()
+{
+    document.getElementById('page_style').setAttribute("href", "style2.css");
+}
